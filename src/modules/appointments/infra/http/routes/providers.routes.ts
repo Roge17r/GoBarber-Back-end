a3ec108cefa898d442898503c721311991012793
Roge Rodrigues <roge17r@gmail.com>
2020-07-26 09:26:09 -0300

    Finishing continuation of app backend

--- src/modules/appointments/infra/http/routes/providers.routes.ts
+++ src/modules/appointments/infra/http/routes/providers.routes.ts
@@ -2,12 +2,26 @@ import { Router } from 'express';
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 import ProvidersController from '../controllers/ProvidersController';
+import ProviderMonthAvailabilityContronller from '../controllers/ProviderMonthAvailabilityContronller';
+import ProviderDayAvailabilityContronller from '../controllers/ProviderDayAvailabilityContronller';
 
 const providersRouter = Router();
+
 const providersController = new ProvidersController();
+const providerMonthAvailabilityContronller = new ProviderMonthAvailabilityContronller();
+const providerDayAvailabilityContronller = new ProviderDayAvailabilityContronller();
 
 providersRouter.use(ensureAuthenticated);
 
 providersRouter.get('/', providersController.index);
+
+providersRouter.get(
+  '/:provider_id/month-availability',
+  providerMonthAvailabilityContronller.index,
+);
+providersRouter.get(
+  '/:provider_id/day-availability',
+  providerDayAvailabilityContronller.index,
+);
 
 export default providersRouter;
